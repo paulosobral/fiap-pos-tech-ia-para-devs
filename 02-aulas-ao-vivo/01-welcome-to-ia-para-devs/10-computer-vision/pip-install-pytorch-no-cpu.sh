cf61f25f4bbad8a44f01e74ee453c7cc70ba1db1
Paulo Frnando Chaves Sobral Junior <paulo.sobral@outlook.com.br>
2026-01-18 16:51:32 -0300

    wip: class 10

--- 02-aulas-ao-vivo/01-welcome-to-ia-para-devs/10-computer-vision/pip-install-pytorch-no-cpu.sh
+++ 02-aulas-ao-vivo/01-welcome-to-ia-para-devs/10-computer-vision/pip-install-pytorch-no-cpu.sh
@@ -1,3 +1,3 @@
-./venv/bin/python -m pip cache purge
-./venv/bin/pip install torch==2.9.0 --index-url https://download.pytorch.org/whl/cpu --no-cache-dir
-./venv/bin/pip install -r requirements.txt --no-cache-dir
+# ./venv/bin/python -m pip cache purge
+./venv/bin/pip install torch==2.9.0 --index-url https://download.pytorch.org/whl/cpu --cache-dir /home/paulo/tmp/
+./venv/bin/pip install -r requirements.txt --cache-dir /home/paulo/tmp/
