12165d4a6673600ba0e6bc540ba278e3c48150eb
Paulo Frnando Chaves Sobral Junior <paulo.sobral@outlook.com.br>
2026-01-18 17:43:38 -0300

    feat: new feature cnn

---
+++ 01-aulas-gravadas/01-welcome-to-ia-para-devs/07-tech-challenge/cnn-computer-vision/pip-install-pytorch-no-cpu.sh
@@ -0,0 +1,3 @@
+# ./venv/bin/python -m pip cache purge
+./venv/bin/pip install torch==2.9.0 --index-url https://download.pytorch.org/whl/cpu --cache-dir /home/paulo/tmp/
+./venv/bin/pip install -r requirements.txt --cache-dir /home/paulo/tmp/
