003e8da64b74f5841bb09c52b6499e34f8a5a8eb
Paulo Sobral <paulo.sobral@outlook.com.br>
2026-02-03 22:44:26 -0300

    wip: class 02

--- 01-aulas-gravadas/01-welcome-to-ia-para-devs/06-computer-vision/02-extracao-de-texto-a-partir-de-imagens-ou-pdf-ocr/exemplo-1/exemplo-1.py
+++ 01-aulas-gravadas/01-welcome-to-ia-para-devs/06-computer-vision/02-extracao-de-texto-a-partir-de-imagens-ou-pdf-ocr/exemplo-1/exemplo-1.py
@@ -4,12 +4,33 @@ import pytesseract
 
 # Carregar a imagem (usando caminho relativo ao script)
 script_dir = os.path.dirname(os.path.abspath(__file__))
-img = cv2.imread(os.path.join(script_dir, 'imagem-1.jpeg'))
+imagem = cv2.imread(os.path.join(script_dir, 'imagem-1.jpeg'))
+
+# converter para grayscale
+# imagem_gray = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
+
+# Exibir imagem grayscale
+# cv2.imshow('Imagem Gray', imagem_gray)
+
+# Redimensionar imagem
+imagem_redimensionada = cv2.resize(imagem, (300, 300))
+
+# Exibir imagem redimensionada
+cv2.imshow('Imagem Redimensionada', imagem_redimensionada)
 
 # Exibir imagem
-cv2.imshow('Imagem', img)
+# cv2.imshow('Imagem', imagem)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-texto = pytesseract.image_to_string(img, lang='por')
+# aplicando o Tesseract OCR
+# texto = pytesseract.image_to_string(imagem, lang='por')
+# print(texto)
+
+# aplicando o Tesseract grayscale
+# texto = pytesseract.image_to_string(imagem_gray, lang='por')
+# print(texto)
+
+# aplicando o Tesseract redimensionada
+texto = pytesseract.image_to_string(imagem_redimensionada, lang='por')
 print(texto)
